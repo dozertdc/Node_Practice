06ede36156ba5375bc2e2cfd117fb0acae40563a
Travis Cooper <dozertdc@gmail.com>
2017-07-16 22:05:15 -1100

    testing objects

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-var greet = require('./greet');
-greet();
+var Person = {
+    firstname: "Travis",
+    lastname: "Cooper",
+    greet: function(){
+        console.log("Hello, " + this.firstname + " " + this.lastname);
+    }
+}
+
+Person.greet();
